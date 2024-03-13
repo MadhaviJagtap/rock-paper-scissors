6d009319276fe96994cfb89c940bf28a144741c4
Madhavi Jagtap <117476112+MadhaviJagtap@users.noreply.github.com>
2024-03-13 12:27:13 +0530

    Update game.py
    optimised code

--- game.py
+++ game.py
@@ -1,44 +1,45 @@
 import random as rd
 import art as a
 
-print(a.logo)
+def get_user_choice():
+    """Get user choice from input."""
+    return int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-user = int(
-    input(
-        "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"
-    ))
-comp = rd.randint(0, 2)
+def display_choices(choice, choice_name):
+    """Display the choice made by the user or the computer."""
+    print(f"\nYou chose {choice_name}!\n{choice}")
 
-if user == 0:
-    print("\nYou chose Rock!\n", a.rock)
-elif user == 1:
-    print("\nYou chose Paper!\n", a.paper)
-elif user == 2:
-    print("\nYou chose Scissors!\n", a.scissors)
-else:
-    print("Enter valid input")
+def main():
+    """Main function to run the Rock-Paper-Scissors game."""
+    print(a.logo)
 
-print("Computer chose:")
-if comp == 0:
-    print("\nRock!\n", a.rock)
-elif comp == 1:
-    print("\nPaper!\n", a.paper)
-elif comp == 2:
-    print("\nScissors!\n", a.scissors)
-else:
-    print("Enter valid input")
+    user = get_user_choice()
+    comp = rd.randint(0, 2)
 
-# rock 0
-# paper 1
-# scissors 2
+    choices = [a.rock, a.paper, a.scissors]
+    choice_names = ["Rock", "Paper", "Scissors"]
 
-if user == comp:
-    print("It's a tie!")
-elif user == "0" and comp == "2":
-    print("You win!")
-elif user == "2" and comp == "1":
-    print("You win!")
-elif user == "1" and comp == "0":
-    print("You win!")
-else:
-    print("You lose!")
+    if 0 <= user <= 2:
+        display_choices(choices[user], choice_names[user])
+    else:
+        print("Enter valid input")
+
+    print("Computer chose:")
+    if 0 <= comp <= 2:
+        display_choices(choices[comp], choice_names[comp])
+    else:
+        print("Enter valid input")
+
+    # rock 0
+    # paper 1
+    # scissors 2
+
+    if user == comp:
+        print("It's a tie!")
+    elif user == 0 and comp == 2 or user == 2 and comp == 1 or user == 1 and comp == 0:
+        print("You win!")
+    else:
+        print("You lose!")
+
+if __name__ == "__main__":
+    main()
